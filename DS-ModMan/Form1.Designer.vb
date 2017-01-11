e45860cdff28f3ef9207a033f2281a3f9b3bf5c8
Wulf2k <wulf2k@gmail.com>
2017-01-11 16:26:01 -0600

    Filling in more of the framework

--- DS-ModMan/Form1.Designer.vb
+++ DS-ModMan/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmModMan
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,74 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lblVer = New System.Windows.Forms.Label()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.txtEXEfile = New System.Windows.Forms.TextBox()
+        Me.lblGAFile = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblVer
+        '
+        Me.lblVer.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblVer.AutoSize = True
+        Me.lblVer.Location = New System.Drawing.Point(568, 467)
+        Me.lblVer.Name = "lblVer"
+        Me.lblVer.Size = New System.Drawing.Size(76, 13)
+        Me.lblVer.TabIndex = 12
+        Me.lblVer.Text = "2016.12.03.22"
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnBrowse.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.btnBrowse.Location = New System.Drawing.Point(576, 9)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(68, 24)
+        Me.btnBrowse.TabIndex = 47
+        Me.btnBrowse.Text = "Browse..."
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'txtEXEfile
+        '
+        Me.txtEXEfile.AllowDrop = True
+        Me.txtEXEfile.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtEXEfile.Location = New System.Drawing.Point(63, 12)
+        Me.txtEXEfile.Name = "txtEXEfile"
+        Me.txtEXEfile.ReadOnly = True
+        Me.txtEXEfile.Size = New System.Drawing.Size(507, 20)
+        Me.txtEXEfile.TabIndex = 45
+        Me.txtEXEfile.Text = "Drag 'n Drop DARKSOULS.exe here"
+        Me.txtEXEfile.WordWrap = False
+        '
+        'lblGAFile
+        '
+        Me.lblGAFile.AutoSize = True
+        Me.lblGAFile.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.lblGAFile.Location = New System.Drawing.Point(7, 15)
+        Me.lblGAFile.Name = "lblGAFile"
+        Me.lblGAFile.Size = New System.Drawing.Size(50, 13)
+        Me.lblGAFile.TabIndex = 46
+        Me.lblGAFile.Text = "EXE File:"
+        '
+        'frmModMan
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(656, 489)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.txtEXEfile)
+        Me.Controls.Add(Me.lblGAFile)
+        Me.Controls.Add(Me.lblVer)
+        Me.Name = "frmModMan"
+        Me.Text = "Wulf's Dark Souls Mod Manager"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents lblVer As Label
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents txtEXEfile As TextBox
+    Friend WithEvents lblGAFile As Label
 End Class
--- DS-ModMan/Form1.vb
+++ DS-ModMan/Form1.vb
@@ -1,3 +1,132 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.IO.Compression
+Imports System.Reflection
+
+Public Class frmModMan
+
+    Shared WithEvents maxProgSnapTimer As System.Timers.Timer
+    Public Const RelDarkSoulsDir = "Dark Souls Prepare to Die Edition\DATA"
+    Public Const SteamGames_Library = "SteamLibrary\steamapps\common"
+    Public Const SteamGames_Main = "Program Files (x86)\Steam\steamapps\common"
+
+    Shared bigEndian = False
+    Shared dataPath As String = ""
+    Shared progressSnapInterval = 1000
+    Shared realCurrentProgress As Integer = 0
+    Shared realCurrentProgressMax As Integer = 0
+
+    Dim exelocs As Hashtable
+    Dim dbglocs As Hashtable = New Hashtable
+    Dim rlslocs As Hashtable = New Hashtable
+    Dim jplocs As Hashtable = New Hashtable
+
+
+
+    Private Sub frmModMan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ScanForDarkSoulsDataDir()
+
+
+
+
+    End Sub
+
+    Public Sub ScanForDarkSoulsDataDir()
+
+        Dim installDirList = New List(Of String)
+
+        Dim allDrives() As DriveInfo = DriveInfo.GetDrives()
+        Dim foundMainInstallDir = False
+        For Each d In allDrives
+            'If we haven't already found a main Steam install, check for that.
+            'Otherwise, only check for SteamLibrary on this drive.
+            If Not foundMainInstallDir Then
+                Dim potentialMainSteamInstall = Path.Combine(d.Name, SteamGames_Main)
+                If Directory.Exists(potentialMainSteamInstall) Then
+                    'This is likely the main steam installation folder.
+                    foundMainInstallDir = True
+                    installDirList.Add(potentialMainSteamInstall)
+                End If
+            End If
+
+            Dim possibleSteamLibDir = Path.Combine(d.Name, SteamGames_Library)
+            If Directory.Exists(possibleSteamLibDir) Then
+                installDirList.Add(possibleSteamLibDir)
+            End If
+        Next
+        For Each d In installDirList
+            Dim possibleDarkSoulsPath = Path.Combine(d, RelDarkSoulsDir)
+            Dim exepath = possibleDarkSoulsPath & "\DarkSouls.exe"
+
+            If File.Exists(possibleDarkSoulsPath) Then
+                txtEXEfile.Text = exepath
+                checkEXE()
+            End If
+        Next
+    End Sub
+    Private Sub txt_DragEnter(sender As Object, e As System.Windows.Forms.DragEventArgs) Handles txtEXEfile.DragEnter
+        e.Effect = DragDropEffects.Copy
+    End Sub
+    Private Sub txt_Drop(sender As Object, e As System.Windows.Forms.DragEventArgs) Handles txtEXEfile.DragDrop
+        Dim file() As String = e.Data.GetData(DataFormats.FileDrop)
+        txtEXEfile.Text = file(0)
+
+        checkEXE()
+    End Sub
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        Dim dlg = New OpenFileDialog()
+        Dim dlgResult = DialogResult.Cancel
+        Dim fileName = ""
+
+        dlg.CheckFileExists = True
+        dlg.CheckPathExists = True
+        dlg.Multiselect = False
+        dlg.InitialDirectory = Environment.CurrentDirectory
+        dlg.Filter = "Executable files (*.exe)|*.exe"
+        dlg.SupportMultiDottedExtensions = True
+        dlg.Title = "Select Dark Souls EXE"
+        dlg.ValidateNames = True
+        dlg.AddExtension = True
+        dlg.AutoUpgradeEnabled = True
+        dlg.FileName = "DARKSOULS.exe"
+
+        dlgResult = dlg.ShowDialog()
+
+        fileName = dlg.FileName
+
+        txtEXEfile.Text = fileName
+    End Sub
+
+    Sub checkEXE()
+        Dim fs = New IO.FileStream(txtEXEfile.Text, IO.FileMode.Open)
+        Dim checkVal As Int32
+
+        checkVal = RInt32(fs, &H80)
+
+        Select Case checkVal
+            Case &HCE9634B4
+                MsgBox("Debug")
+            Case &HFC293654
+                MsgBox("NA Release")
+            Case Else
+                MsgBox("Unknown")
+        End Select
+
+        fs.Close()
+    End Sub
+
+    Function RInt32(fs As Stream, ByVal loc As Integer)
+        Dim tmpInt32 As Integer = 0
+        Dim byt = New Byte() {0, 0, 0, 0}
+
+        fs.Position = loc
+
+        fs.Read(byt, 0, 4)
+
+        If bigEndian Then byt.Reverse
+
+        tmpInt32 = BitConverter.ToInt32(byt, 0)
+
+        Return tmpInt32
+    End Function
 
 End Class
